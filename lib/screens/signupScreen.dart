c9f13764eead9bdd072a2b66169b54d3b36fa666
Harrem <52822616+Harrem@users.noreply.github.com>
2022-07-26 15:56:44 +0300

    some adjustments

--- lib/screens/signupScreen.dart
+++ lib/screens/signupScreen.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter/material.dart';
 import 'package:kurdwork/myWidgets.dart';
 import 'package:kurdwork/screens/homeScreen.dart';
@@ -13,6 +15,8 @@ class SignupScreen extends StatefulWidget {
 }
 
 class _SignupScreenState extends State<SignupScreen> {
+  final _auth = FirebaseAuth.instance;
+  final _googleSignIn = GoogleSignIn();
   String email = "", password = "";
   final _formKey = GlobalKey<FormState>();
   @override
@@ -30,22 +34,53 @@ class _SignupScreenState extends State<SignupScreen> {
                   const SizedBox(height: 30),
                   MyWidgets.h3("دروستکردن لەگەڵ", color: Colors.grey),
                   const SizedBox(height: 10),
-                  MyWidgets.outlineButton(
-                    text: "گۆگڵ",
-                    icon: Image.asset(
-                      "assets/icons/google.png",
-                      width: 30,
-                    ),
-                    onPressed: () {},
-                  ),
-                  const SizedBox(height: 10),
-                  MyWidgets.outlineButton(
-                    text: "فەیسبووک",
-                    icon: Image.asset(
-                      "assets/icons/facebook.png",
-                      width: 30,
-                    ),
-                    onPressed: () {},
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(15),
+                        child: SizedBox(
+                          height: 60,
+                          width: 60,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              FirebaseAuth.instance
+                                  .signInWithAuthProvider(GoogleAuthProvider());
+                            },
+                            style: ElevatedButton.styleFrom(
+                                primary:
+                                    const Color.fromARGB(255, 255, 211, 211),
+                                elevation: 0,
+                                fixedSize: const Size(60, 60),
+                                padding: const EdgeInsets.all(9)),
+                            clipBehavior: Clip.none,
+                            child: SizedBox(
+                              height: 55,
+                              width: 55,
+                              child: Image.asset("assets/icons/google.png"),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(15),
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                              primary: const Color.fromARGB(255, 221, 247, 255),
+                              elevation: 0,
+                              fixedSize: const Size(60, 60),
+                              padding: const EdgeInsets.all(7)),
+                          clipBehavior: Clip.none,
+                          child: SizedBox(
+                            height: 50,
+                            width: 50,
+                            child: Image.asset("assets/icons/facebook.png"),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                   const SizedBox(height: 40),
                   MyWidgets.h3("دروستکردن بە ئیمەیل یان ژمارە مۆبایل",
@@ -55,40 +90,43 @@ class _SignupScreenState extends State<SignupScreen> {
                       key: _formKey,
                       child: Column(
                         children: [
-                          TextFormField(
-                            decoration: InputDecoration(
-                              labelText: "ئیمەیل",
-                              labelStyle: TextStyle(color: Colors.grey),
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: TextFormField(
+                              decoration: InputDecoration(
+                                labelText: "ئیمەیل",
+                                labelStyle: TextStyle(color: Colors.grey),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
                               ),
+                              validator: (value) {
+                                if (value == "") {
+                                  return "please enter your email or phone number";
+                                }
+                              },
+                              onChanged: (value) {
+                                email = value;
+                              },
                             ),
-                            validator: (value) {
-                              if (value == "") {
-                                return "please enter your email or phone number";
-                              }
-                            },
-                            onChanged: (value) {
-                              email = value;
-                            },
                           ),
-                          TextFormField(
-                            decoration: InputDecoration(
-                              labelText: "ژمارەی مۆبایل",
-                              labelStyle: TextStyle(color: Colors.grey),
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: TextFormField(
+                              decoration: InputDecoration(
+                                labelText: "وشەی تێپەر",
+                                labelStyle: TextStyle(color: Colors.grey),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
                               ),
+                              onChanged: (value) {
+                                password = value;
+                              },
                             ),
-                            onChanged: (value) {
-                              password = value;
-                            },
                           ),
                         ],
                       )),
-                  const SizedBox(height: 10),
-                  MyWidgets.myTextField(context,
-                      labelText: "وشەی تێپەر", width: 330),
                   const SizedBox(height: 30),
                   MyWidgets.myElevatedButton(
                     context,
