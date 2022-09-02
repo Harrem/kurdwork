d9943aa897d41a13b6867cdd125c5a12e460c7f3
Harrem <52822616+Harrem@users.noreply.github.com>
2022-09-02 13:11:24 +0300

    fixed window size

--- lib/screens/profile_screens/add_education.dart
+++ lib/screens/profile_screens/add_education.dart
@@ -5,112 +5,84 @@ class AddEducation extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Material(
-        color: Colors.transparent,
+    return Material(
+      color: Colors.transparent,
+      child: Center(
         child: Container(
           padding: const EdgeInsets.all(20),
-          child: Card(
-            child: Container(
-              padding: const EdgeInsets.all(20),
-              width: double.infinity,
-              child: Column(
+          margin: const EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          height: 460,
+          child: Column(
+            children: [
+              Text(
+                "Add Education",
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              Divider(
+                color: Colors.grey[200],
+              ),
+              const Divider(),
+              TextField(
+                decoration: InputDecoration(
+                    fillColor: Colors.grey[300],
+                    labelText: 'School',
+                    hintText: "Ex. Charmo University"),
+              ),
+              const Divider(height: 20),
+              Row(
                 children: [
-                  Text(
-                    "Add Education",
-                    style: Theme.of(context).textTheme.headline6,
-                  ),
-                  Divider(
-                    color: Colors.grey[200],
-                  ),
-                  const Divider(),
-                  Container(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      'school',
-                      style: Theme.of(context).textTheme.subtitle1,
-                    ),
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
                         fillColor: Colors.grey[300],
-                        hintText: "Ex. Charmo University"),
-                  ),
-                  const SizedBox(height: 30),
-                  Container(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      "Dates Attended",
-                      style: Theme.of(context).textTheme.subtitle1,
-                    ),
-                  ),
-                  const Divider(),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: TextField(
-                          decoration: InputDecoration(
-                            fillColor: Colors.grey[300],
-                            hintText: 'Date Started',
-                          ),
-                        ),
+                        labelText: "Dates Attended",
+                        hintText: 'Date Started',
                       ),
-                      const VerticalDivider(),
-                      Expanded(
-                        child: TextField(
-                          decoration: InputDecoration(
-                            fillColor: Colors.grey[300],
-                            hintText: 'Date Ended',
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                  const SizedBox(height: 30),
-                  Container(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      "Degree",
-                      style: Theme.of(context).textTheme.subtitle1,
                     ),
                   ),
-                  const Divider(),
-                  TextField(
-                    decoration: InputDecoration(
+                  const VerticalDivider(),
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
                         fillColor: Colors.grey[300],
-                        hintText: "Ex: Master of Computer Science"),
-                  ),
-                  const SizedBox(height: 30),
-                  Container(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      "Field of study",
-                      style: Theme.of(context).textTheme.subtitle1,
+                        hintText: 'Date Ended',
+                      ),
                     ),
-                  ),
-                  const Divider(),
-                  TextField(
-                    decoration: InputDecoration(
-                        fillColor: Colors.grey[300],
-                        hintText: "Ex. Applied Computer Science"),
-                  ),
-                  const SizedBox(height: 30),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      OutlinedButton(
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          child: const Text("Cancel")),
-                      const VerticalDivider(),
-                      ElevatedButton(
-                          onPressed: () {}, child: const Text("Done")),
-                    ],
-                  ),
+                  )
                 ],
               ),
-            ),
+              const Divider(height: 20),
+              TextField(
+                decoration: InputDecoration(
+                    fillColor: Colors.grey[300],
+                    labelText: "Degree",
+                    hintText: "Ex: Master of Computer Science"),
+              ),
+              const Divider(height: 20),
+              TextField(
+                decoration: InputDecoration(
+                    fillColor: Colors.grey[300],
+                    labelText: "Field of Studyhjdskf",
+                    hintText: "Ex. Applied Computer Science"),
+              ),
+              const Divider(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  OutlinedButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Text("Cancel")),
+                  const VerticalDivider(),
+                  ElevatedButton(onPressed: () {}, child: const Text("Done")),
+                ],
+              ),
+            ],
           ),
         ),
       ),
--- lib/screens/profile_screens/add_skill.dart
+++ lib/screens/profile_screens/add_skill.dart
@@ -3,98 +3,95 @@ import 'package:kurdwork/widgets/custom_widgets.dart';
 
 import '../../mockData/users.dart';
 
-class AddSkill extends StatelessWidget {
-  AddSkill({Key? key}) : super(key: key);
-  var controller = TextEditingController();
+class EditSkills extends StatelessWidget {
+  EditSkills({Key? key}) : super(key: key);
+  final TextEditingController controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.transparent,
-      child: Container(
-        padding: const EdgeInsets.all(20),
-        child: Card(
-          color: Colors.white,
-          child: Container(
-            color: Colors.amber,
-            width: double.infinity,
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              children: [
-                Text(
-                  "Add New Skills",
-                  style: Theme.of(context).textTheme.headline6,
+      child: Center(
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          margin: const EdgeInsets.all(20),
+          alignment: Alignment.topCenter,
+          height: 470,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                "Add New Skills",
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              Divider(color: Colors.grey[200]),
+              Container(
+                width: double.infinity,
+                height: 260,
+                decoration: BoxDecoration(
+                  color: Colors.grey[100],
+                  borderRadius: BorderRadius.circular(5),
                 ),
-                Divider(color: Colors.grey[200]),
-                Container(
-                  width: double.infinity,
-                  height: 228,
-                  decoration: BoxDecoration(
-                    color: Colors.grey[100],
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: SingleChildScrollView(
-                      child: Wrap(
-                        spacing: 15,
-                        runSpacing: 15,
-                        clipBehavior: Clip.hardEdge,
-                        children: List.generate(
-                          (users[0]['skills'] as List<String>).length,
-                          (index) => Stack(
-                            alignment: Alignment.centerRight,
-                            children: [
-                              CustomChip(
-                                padding:
-                                    const EdgeInsets.fromLTRB(10, 7, 30, 5),
-                                label: Text((users[0]['skills']
-                                    as List<String>)[index]),
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: SingleChildScrollView(
+                    child: Wrap(
+                      spacing: 15,
+                      runSpacing: 15,
+                      clipBehavior: Clip.hardEdge,
+                      children: List.generate(
+                        (users[0]['skills'] as List<String>).length,
+                        (index) => Stack(
+                          alignment: Alignment.centerRight,
+                          children: [
+                            CustomChip(
+                              padding: const EdgeInsets.fromLTRB(10, 7, 30, 5),
+                              label: Text(
+                                  (users[0]['skills'] as List<String>)[index]),
+                            ),
+                            IconButton(
+                              onPressed: () {},
+                              icon: const Icon(
+                                Icons.remove_circle,
+                                size: 20,
+                                color: Colors.grey,
                               ),
-                              IconButton(
-                                onPressed: () {},
-                                icon: const Icon(
-                                  Icons.remove_circle,
-                                  size: 20,
-                                  color: Colors.redAccent,
-                                ),
-                              ),
-                            ],
-                          ),
+                            ),
+                          ],
                         ),
                       ),
                     ),
                   ),
                 ),
-                const Divider(),
-                TextField(
-                  decoration: InputDecoration(
-                      fillColor: Colors.grey[100], hintText: "Add Skill"),
-                ),
-                const Divider(),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    SizedBox(
-                      height: 44,
-                      child: OutlinedButton(
-                        onPressed: () {
-                          String skill;
-                          skill = controller.text;
-                        },
-                        child: const Text("Add"),
-                      ),
-                    ),
-                    VerticalDivider(width: 10),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      child: const Text("Done"),
-                    ),
-                  ],
-                ),
-              ],
-            ),
+              ),
+              const Divider(),
+              TextField(
+                decoration: InputDecoration(
+                    fillColor: Colors.grey[100], hintText: "Add Skill"),
+              ),
+              const Divider(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  OutlinedButton(
+                    onPressed: () {
+                      String skill = controller.text;
+                    },
+                    child: const Text("Add"),
+                  ),
+                  const VerticalDivider(width: 10),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text("Done"),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
