ce99cea4c87ffa6615f6f1d4a7bd6bc414aea180
Harrem <52822616+Harrem@users.noreply.github.com>
2022-09-06 21:03:20 +0300

    add initialize user method

--- lib/controller/profile_settings.dart
+++ lib/controller/profile_settings.dart
@@ -5,34 +5,29 @@ import 'package:file_picker/file_picker.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/foundation.dart';
-import 'package:kurdwork/Models/user.dart' as model;
-import 'package:kurdwork/mockData/userMockData.dart';
+import 'package:kurdwork/Models/user.dart';
 
 class UserActions {
   FirebaseFirestore firestore = FirebaseFirestore.instance;
   FirebaseStorage firebaseStorage = FirebaseStorage.instance;
   FirebaseAuth firebaseAuth = FirebaseAuth.instance;
 
-  Future<model.User> initializeUser() async {
-    model.User user;
+  Future<UserData?> initializeUser() async {
+    UserData? userData;
     var documnt =
         firestore.collection('users').doc(firebaseAuth.currentUser!.uid);
     var map = await documnt.get();
-    if (map.data() != null) {
-      user = model.User.fromMap(map.data()!);
-      debugPrint("user initialized from live data ${map.data()!}");
+    if (map.data()!.entries.isNotEmpty) {
+      userData = UserData.fromMap(map.data()!);
+      debugPrint("user data initialized from live data ${map.data()!.entries}");
     } else {
-      user = model.User.fromJson(userMock.toString());
-      debugPrint("user initialized from mock data! ${map.data()!}");
-    }
-    return user;
-  }
-
-  Future<void> checkFirstTimeUser(User user) async {
-    var data = await firestore.collection("users").doc(user.uid).get();
-    if (data.data() != null || data.data()!.isEmpty) {
       debugPrint("!!!!!!!!!!!!!!!     is First Time user      !!!!!!!!!!!!!");
     }
+    return userData;
+  }
+
+  Future<UserData> createProfile() async {
+    return UserData(uid: "jsdfs");
   }
 
   Future<void> updateName(String fname, String lname, String uid) async {
